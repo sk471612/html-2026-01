9d47600e4891d97048ab03511c435ba05934d25c
sk471612 <daejung1623@dge.go.kr>
2026-05-07 15:03:44 +0900

    Add files via upload

---
+++ TriangleLB.java
@@ -0,0 +1,19 @@
+package java;
+import java.util.Scanner;
+
+public class TriangleLB {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("높이: ");
+		int num = sc.nextInt();
+		
+		for(int i =1; i <= num ; i++) {
+			for(int j = 1; j <= 1; j++) {
+				System.out.print("*");
+			
+			}
+			System.out.println("\n");
+		}
+
+	}
+}
